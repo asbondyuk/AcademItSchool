b27dfb94eec2c2cec137de78b2ecf1d60062a347
Aleksander Bondyuk <aleksander.bondyuk@yandex.ru>
2020-02-06 16:38:08 +0700

    Задача Vector, пункт 4: реализованы нестатистические методы (без обработки исключений):  прибавление/вычитание вектора, умножение на скаляр,  разворот, получение длины,  получение/установка элемента по индексу,  переопределены equals и hashCode с учетом размерности векторов

--- Vector/src/ru/academits/bondyuk/vector/Main.java
+++ Vector/src/ru/academits/bondyuk/vector/Main.java
@@ -3,9 +3,31 @@ package ru.academits.bondyuk.vector;
 public class Main {
     public static void main(String[] args) {
         Vector vector1 = new Vector(4);
+
         System.out.println(vector1.toString());
 
-//        Vector vector2 = new Vector(-1);
-//        System.out.println(vector2);
+        Vector vector2 = new Vector(new double[]{1, 1, 1, 1, 1, 1, 1, 1, 1});
+
+        System.out.println(vector2.toString());
+        System.out.printf("Длина вектора: %.2f\n", vector2.getLength());
+
+        vector2.increaseByNumber(5.5);
+        System.out.println(vector2.toString());
+
+        vector2.reverse();
+        System.out.println(vector2.toString());
+
+        vector2.setElement(2,10);
+        System.out.println(vector2.toString());
+
+        System.out.println(vector2.getElement(2));
+
+        Vector vector3 = new Vector(vector2);
+
+        vector2.addVector(vector3);
+        System.out.println(vector2.toString());
+
+        vector3.differenceVector(vector2);
+        System.out.println(vector3.toString());
     }
 }
--- Vector/src/ru/academits/bondyuk/vector/Vector.java
+++ Vector/src/ru/academits/bondyuk/vector/Vector.java
@@ -29,8 +29,85 @@ public class Vector {
         return components;
     }
 
+    public void setComponents(double[] components) {
+        this.components = components;
+    }
+
     public int getSize() {
         return components.length;
+    }
+
+    public void addVector(Vector vector) {
+        int newComponentsLength = Math.max(components.length, vector.getComponents().length);
+        double[] newComponents = new double[newComponentsLength];
+
+        for (int i = 0; i < newComponentsLength; ++i) {
+            double firstTerm = 0;
+            if (i < components.length) {
+                firstTerm = components[i];
+            }
+
+            double secondTerm = 0;
+            if (i < vector.getComponents().length) {
+                secondTerm = vector.getComponents()[i];
+            }
+
+            newComponents[i] = firstTerm + secondTerm;
+        }
+
+        setComponents(newComponents);
+    }
+
+
+    public void differenceVector(Vector vector) {
+        int newComponentsLength = Math.max(components.length, vector.getComponents().length);
+        double[] newComponents = new double[newComponentsLength];
+
+        for (int i = 0; i < newComponentsLength; ++i) {
+            double firstTerm = 0;
+            if (i < components.length) {
+                firstTerm = components[i];
+            }
+
+            double secondTerm = 0;
+            if (i < vector.getComponents().length) {
+                secondTerm = vector.getComponents()[i];
+            }
+
+            newComponents[i] = firstTerm - secondTerm;
+        }
+
+        setComponents(newComponents);
+    }
+
+    public void increaseByNumber(double number) {
+        double[] newComponents = new double[components.length];
+
+        for (int i = 0; i < components.length; ++i) {
+            newComponents[i] = components[i] * number;
+        }
+
+        setComponents(newComponents);
+    }
+
+    public void reverse() {
+        increaseByNumber(-1);
+    }
+
+    public double getLength() {
+        double componentsSquaredSum = 0;
+        for (double component : components) {
+            componentsSquaredSum += Math.pow(component, 2);
+        }
+        return Math.sqrt(componentsSquaredSum);
+    }
+
+    public double getElement(int index) {
+        return components[index];
+    }
+
+    public void setElement(int index, double number) {
+        components[index] = number;
     }
 
     @Override
@@ -48,5 +125,21 @@ public class Vector {
         stringBuilder.append("}");
 
         return stringBuilder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector vector = (Vector) o;
+
+        return Arrays.equals(components, vector.components) && (components.length == vector.getComponents().length);
+    }
+
+    @Override
+    public int hashCode() {
+        int componentsHash = Arrays.hashCode(components);
+
+        return 31 * componentsHash + components.length;
     }
 }

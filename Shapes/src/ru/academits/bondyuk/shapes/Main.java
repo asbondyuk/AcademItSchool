79b85eb3b8ffa46be9e7053baa03312bcb714ae6
Aleksander Bondyuk <aleksander.bondyuk@yandex.ru>
2020-02-06 12:46:02 +0700

    задача Shapes часть 2: отображение приведено к требованиям по условию (вывод данных о фигуре)

--- Shapes/src/ru/academits/bondyuk/shapes/Main.java
+++ Shapes/src/ru/academits/bondyuk/shapes/Main.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class Main {
+    public static void printShapeInformation(Shape shape) {
+        System.out.printf("%s\nПлощадь: %f, \nПериметр: %f, \nВысота: %f, \nШирина: %f \n",
+                shape.getClass().getSimpleName(), shape.getArea(), shape.getPerimeter(), shape.getHeight(), shape.getWidth());
+    }
+
     public static void main(String[] args) {
         ArrayList<Shape> shapes = new ArrayList<>();
 
@@ -13,20 +18,28 @@ public class Main {
         shapes.add(new Rectangle(60, 4));
         shapes.add(new Rectangle(4, 4));
 
-        // Поверка площади
         for (Shape shape : shapes) {
-            System.out.println(shape.getClass() + "  площадь составляет: " + shape.getArea() + ", периметр: " + shape.getPerimeter());
+            System.out.printf("%s площадь составляет: %.2f, периметр: %.2f\n", shape.getClass().getSimpleName(), shape.getArea(), shape.getPerimeter());
         }
 
+        // Поверка площади
         Comparator<Shape> areaComparator = Comparator.comparing(Shape::getArea);
 
         shapes.sort(areaComparator);
-        System.out.println("\nМаксимальная площадь: " + shapes.get(shapes.size() - 1).getArea());
+
+        System.out.println("\nМаксимальная площадь у фигуры: ");
+
+        int maxAreaShapeIndex = shapes.size() - 1;
+        printShapeInformation(shapes.get(maxAreaShapeIndex));
 
         // Проверка периметра
         Comparator<Shape> perimeterComparator = Comparator.comparing(Shape::getPerimeter);
 
         shapes.sort(perimeterComparator);
-        System.out.println("\nВторой по величине периметр: " + shapes.get(shapes.size() - 2).getPerimeter());
+
+        System.out.println("\nВторой по величине периметр у фигуры: ");
+
+        int secondMaxPerimeterShapeIndex = shapes.size() - 2;
+        printShapeInformation(shapes.get(secondMaxPerimeterShapeIndex));
     }
 }

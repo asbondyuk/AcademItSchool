af4474e5ec056b9680b4b6629e3acf93c01a305f
aleksander bondyuk <aleksander.bondyuk@yandex.ru>
2020-04-02 10:29:15 +0700

    CSV: форматирование/кодстайл

--- CSV/src/ru/academits/bondyuk/csv_parser/Parser.java
+++ CSV/src/ru/academits/bondyuk/csv_parser/Parser.java
@@ -5,99 +5,16 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Parser {
-    static final String defaultDelimiter = ","; // для split в дальнейшем
-    static final char quotationMarks = '\"';
-
-    private static String handleSpecialCharacter(String string) {
-        return string.replace(">", "&gt;")
-                .replace("<", "&lt;")
-                .replace("&", "&amp;");
-    }
-
-    private static boolean isRowEnd(String line) {
-        return countQuotes(line) % 2 == 0;
-    }
-
-    private static int countQuotes(String line) {
-        int quotesCount = 0;
-
-        for (int i = 0; i < line.length(); ++i) {
-            if (line.charAt(i) == '"') {
-                ++quotesCount;
-            }
-        }
-
-        return quotesCount;
-    }
-
-    private static List<String> splitLine(String line) {
-        List<String> strings = new ArrayList<>();
-
-        boolean isQuotesCell = false;
-        int startIndex = 0;
-
-        for (int i = 0; i < line.length() - 1; ++i) {
-            if (line.charAt(0) == '"') {
-                isQuotesCell = true;
-                continue;
-            }
-
-            if (line.charAt(i) == '"' & line.charAt(i + 1) == '"' & isQuotesCell) {
-                ++i;
-                continue;
-            }
-
-            if (line.charAt(i) == '"' & line.charAt(i + 1) == ',' & isQuotesCell) {
-                strings.add(line.substring(startIndex, i));
-                startIndex = i + 2;
-                isQuotesCell = false;
-                ++i;
-                continue;
-            }
-
-            if (line.charAt(i) == ',' & line.charAt(i + 1) == '"' & !isQuotesCell) {
-                strings.add(line.substring(startIndex, i));
-                startIndex = i + 2;
-                isQuotesCell = true;
-                ++i;
-                continue;
-            }
-
-            if (i + 2 == line.length() & line.charAt(i + 1) == '"') {
-                strings.add(line.substring(startIndex + 1, line.length() - 2));
-                continue;
-            }
-
-            if (!isQuotesCell & line.charAt(i) == ',') {
-                strings.add(line.substring(startIndex, i));
-                startIndex = i + 1;
-            }
-        }
-
-        if (line.charAt(line.length() - 1) != '"') {
-            strings.add(line.substring(startIndex));
-        }
-
-
-        for (int i = 0; i < strings.size(); ++i) {
-            if (strings.get(i).contains("\"\"")) {
-                strings.set(i, strings.get(i).replace("\"\"", "\""));
-                ++i;
-            }
-        }
-
-        return strings;
-    }
+    static final char defaultDelimiter = ',';
+    static final char quotationMarks = '"';
 
     public static void parseCSV(String inputFileName, String outputFileName) {
         try (Scanner scanner = new Scanner(new FileInputStream(inputFileName));
              PrintWriter printWriter = new PrintWriter(new File(outputFileName))) {
-
             printWriter.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\n" +
                     "<html>\n" +
                     " <head>\n" +
@@ -137,8 +54,6 @@ public class Parser {
                 line = handleSpecialCharacter(line);
                 cellsValues = splitLine(line);
 
-                System.out.println(cellsValues);
-
                 for (String value : cellsValues) {
                     printWriter.println("<td>" + value + "</td>");
                 }
@@ -150,6 +65,96 @@ public class Parser {
             printWriter.println("</body>");
         } catch (FileNotFoundException e) {
             System.err.println(e.getMessage());
+        }
+    }
+
+    private static boolean isRowEnd(String line) {
+        return countQuotes(line) % 2 == 0;
+    }
+
+    private static int countQuotes(String line) {
+        int quotesCount = 0;
+
+        for (int i = 0; i < line.length(); ++i) {
+            if (line.charAt(i) == quotationMarks) {
+                ++quotesCount;
+            }
+        }
+
+        return quotesCount;
+    }
+
+    private static String handleSpecialCharacter(String string) {
+        return string.replace(">", "&gt;")
+                .replace("<", "&lt;")
+                .replace("&", "&amp;");
+    }
+
+    private static List<String> splitLine(String line) {
+        List<String> strings = new ArrayList<>();
+
+        boolean isQuotesCell = false;
+        int startIndex = 0; // начальный индекс элемента для записи ячейки
+
+        // провека разделителей
+        for (int i = 0; i < line.length() - 1; ++i) {
+            if (line.charAt(0) == quotationMarks) {
+                isQuotesCell = true;
+                continue;
+            }
+
+            if (isQuotesCell & line.charAt(i) == quotationMarks & line.charAt(i + 1) == quotationMarks) {
+                ++i;
+                continue;
+            }
+
+            if (isQuotesCell & line.charAt(i) == quotationMarks & line.charAt(i + 1) == defaultDelimiter) {
+                strings.add(line.substring(startIndex, i));
+
+                startIndex = i + 2;
+                isQuotesCell = false;
+                ++i;
+                continue;
+            }
+
+            if (line.charAt(i) == defaultDelimiter & line.charAt(i + 1) == quotationMarks & !isQuotesCell) {
+                strings.add(line.substring(startIndex, i));
+
+                startIndex = i + 2;
+                isQuotesCell = true;
+                ++i;
+                continue;
+            }
+
+            if (i + 2 == line.length() & line.charAt(i + 1) == quotationMarks) {
+                strings.add(line.substring(startIndex + 1, line.length() - 2));
+
+                continue;
+            }
+
+            if (!isQuotesCell & line.charAt(i) == ',') {
+                strings.add(line.substring(startIndex, i));
+
+                startIndex = i + 1;
+            }
+        }
+
+        // добавляем последюю ячейку, без разделителя
+        if (line.charAt(line.length() - 1) != quotationMarks) {
+            strings.add(line.substring(startIndex));
+        }
+
+        getWithoutQuotesEscaping(strings);
+
+        return strings;
+    }
+
+    private static void getWithoutQuotesEscaping(List<String> stringsList) {
+        for (int i = 0; i < stringsList.size(); ++i) {
+            if (stringsList.get(i).contains("\"\"")) {
+                stringsList.set(i, stringsList.get(i).replace("\"\"", "\""));
+                ++i;
+            }
         }
     }
 }

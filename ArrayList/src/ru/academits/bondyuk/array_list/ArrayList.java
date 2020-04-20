221a3586df544d55c75a54ab3ba0026d263ac6b9
aleksander bondyuk <aleksander.bondyuk@yandex.ru>
2020-04-20 09:26:23 +0700

    ArrayList: 1) Добавлен конструктор от array 2) Добавлен конструтор по размерности массива 3) убраны предупреждения при приведении типов 4) Iterator: реализован remove 5) реализация toArray([] array) 6) реализация ensureCapacity(int minCapacity) 7) реализация increaseCapacity() 8) реализация trimToSize() 9) реализация add(E e) 10) реализация remove(Object o) 11) реализация containsAll(Collection) 12) реализация addAll(Collection) 13) реализация addAll(int index, Collection) 14) реализация removeAll(Collection) 15) реализация retainAll(Collection) 16) реализация clear() 17) вспомогательный метод checkIndex(int index) 18) добавлены проверки индексов в методах 19) реализация add(int index, E e) 20) реализация remove(int index) 21) реализация indexOf/lastIndexOf(Object o) 22) реализация toString

--- ArrayList/src/ru/academits/bondyuk/array_list/ArrayList.java
+++ ArrayList/src/ru/academits/bondyuk/array_list/ArrayList.java
@@ -3,11 +3,28 @@ package ru.academits.bondyuk.array_list;
 import java.util.*;
 
 public class ArrayList<E> implements List<E> {
+    private static final int DEFAULT_CAPACITY = 10;
+
     private E[] items;
     private int size;
     private int modCount; // число изменений
 
+    @SuppressWarnings("unchecked")
     public ArrayList() {
+        items = (E[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
+        modCount = 0;
+    }
+
+    public ArrayList(E[] array) {
+        items = array;
+        size = array.length;
+        modCount = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public ArrayList(int capacity) {
+        items = (E[]) new Object[capacity];
         size = 0;
         modCount = 0;
     }
@@ -40,6 +57,11 @@ public class ArrayList<E> implements List<E> {
 
             return items[currentIndex];
         }
+
+        @Override
+        public void remove() {
+            items[currentIndex] = null;
+        }
     }
 
     @Override
@@ -57,7 +79,7 @@ public class ArrayList<E> implements List<E> {
         Iterator<E> iterator = new ArrayListIterator();
 
         while (iterator.hasNext()) {
-            if (iterator.next().equals(o)) {
+            if (Objects.equals(iterator.next(), o)) {
                 return true;
             }
         }
@@ -73,104 +95,233 @@ public class ArrayList<E> implements List<E> {
     @Override
     public Object[] toArray() {
         Object[] objects = new Object[size];
-
-        for (int i = 0; i < size; ++i) {
-            objects[i] = items[i];
-        }
+        System.arraycopy(items, 0, objects, 0, size);
 
         return objects;
     }
 
     @Override
-    public <T> T[] toArray(T[] a) {
-//        E[] array = items;
-//
-//        for (int i = 0; i < size; ++i) {
-//            objects[i] = items[i];
-//        }
+    @SuppressWarnings("unchecked")
+//    смысл данного метода не ясен
+    public <T> T[] toArray(T[] array) {
+        T[] intersectionArray = (T[]) new Object[array.length];
+        int intersectionCount = 0;
 
-        return null;
+        for (E item : items) {
+            for (T t : array) {
+                if (Objects.equals(item, t)) {
+                    intersectionArray[intersectionCount++] = t;
+                    break;
+                }
+            }
+        }
+
+        return intersectionArray;
+    }
+
+    public void ensureCapacity(int minCapacity) {
+        if (items.length < minCapacity) {
+            int increasingCoefficient = (int) Math.ceil((double) minCapacity / items.length);
+            items = Arrays.copyOf(items, items.length * increasingCoefficient);
+        }
+    }
+
+    public void increaseCapacity() {
+        items = Arrays.copyOf(items, items.length * 2);
+        ++modCount;
+    }
+
+    public void trimToSize() {
+        if (size < items.length - 1) {
+            items = Arrays.copyOf(items, size);
+        }
+
+        ++modCount;
     }
 
     @Override
     public boolean add(E e) {
+        if (size >= items.length) {
+            increaseCapacity();
+        }
+
+        items[size] = e;
         ++modCount;
+        ++size;
 
-
-        return false;
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
+        Iterator<E> iterator = iterator();
+
+        while (iterator.hasNext()) {
+            if (Objects.equals(iterator.next(), o)) {
+                iterator.remove();
+
+                return true;
+            }
+        }
+
         ++modCount;
+        --size;
+
         return false;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        for (Object o : c) {
+            if (!contains(o)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
+        int newListSize = size + c.size();
+        if (newListSize >= items.length) {
+            ensureCapacity(newListSize);
+        }
+
+        for (E e : c) {
+            add(e);
+        }
+
         ++modCount;
-        return false;
+        return true;
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
+        int newListSize = size + c.size();
+        if (newListSize >= items.length) {
+            ensureCapacity(newListSize);
+        }
+
+        int addedPosition = index;
+        for (E e : c) {
+            add(addedPosition, e);
+            ++addedPosition;
+        }
+
         ++modCount;
         return false;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
+        for (Object o : c) {
+            remove(o);
+        }
+
         ++modCount;
         return false;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
+        removeIf(e -> !c.contains(e));
+//        оптимизация idea периодически пугает. Первоначальный код:
+//        while (iterator.hasNext()) {
+//            if (!c.contains(iterator.next())) {
+//                iterator.remove();
+//            }
+//        }
         ++modCount;
-        return false;
+        return true;
     }
 
     @Override
     public void clear() {
+        Arrays.fill(items, null);
+        size = 0;
         ++modCount;
+    }
+
+    private void checkIndex(int index) {
+        if ((index < 0) && (index >= size)) {
+            throw new ArrayIndexOutOfBoundsException("Некорректный индекс списка, должно быть значение от 0 до " + (size - 1)
+                    + ". Получен: " + index);
+        }
     }
 
     @Override
     public E get(int index) {
+        checkIndex(index);
+
         return items[index];
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        checkIndex(index);
+
+        E oldValue = items[index];
+        items[index] = element;
+
+        return oldValue;
     }
 
     @Override
     public void add(int index, E element) {
+        checkIndex(index);
+
+        if (size >= items.length - 1) {
+            increaseCapacity();
+        }
+
+        if (size - index >= 0) {
+            System.arraycopy(items, index, items, index + 1, size - index);
+        }
+
+        items[index] = element;
         ++modCount;
+        ++size;
     }
 
     @Override
     public E remove(int index) {
-        ++modCount;
-        return null;
+        checkIndex(index);
+
+        E oldValue = items[index];
+        items[index] = null;
+
+        return oldValue;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        for (int i = 0; i < items.length; ++i) {
+            if (Objects.equals(items[i], o)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        for (int i = items.length - 1; i >= 0; --i) {
+            if (Objects.equals(items[i], o)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
+    @Override
+    public String toString() {
+        return Arrays.toString(items);
+    }
+
+    // последующие методы не требуют реализации по условиям задачи
     @Override
     public ListIterator<E> listIterator() {
         return null;
--- ArrayList/src/ru/academits/bondyuk/array_list_main/Main.java
+++ ArrayList/src/ru/academits/bondyuk/array_list_main/Main.java
@@ -1,4 +1,256 @@
 package ru.academits.bondyuk.array_list_main;
 
+import ru.academits.bondyuk.array_list.ArrayList;
+
+import java.util.*;
+
 public class Main {
+    public static boolean testAdd() {
+        ArrayList<String> list = new ArrayList<>();
+        return list.add("a");
+    }
+
+    public static ArrayList<String> createArrayList() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        list.add(null);
+        list.add("b");
+        list.add("c");
+        list.add("d");
+
+        return list;
+    }
+
+    public static boolean testSize() {
+        return createArrayList().size() == 5;
+    }
+
+    public static boolean testToString() {
+        return createArrayList().toString().equals("[a, null, b, c, d, null, null, null, null, null]");
+    }
+
+    public static boolean testAddFirst() {
+        ArrayList<String> list = createArrayList();
+        list.add(0, "f");
+
+        return list.toString().equals("[f, a, null, b, c, d, null, null, null, null]");
+    }
+
+    public static boolean testAddLast() {
+        ArrayList<String> list = createArrayList();
+        list.add(list.size(), "f");
+
+        return list.toString().equals("[a, null, b, c, d, f, null, null, null, null]");
+    }
+
+    public static boolean testRemoveByIndex() {
+        int index = 2;
+        return createArrayList().remove(index).equals("b");
+    }
+
+    public static boolean testEnsureCapacity() {
+        int capacity = 15;
+        ArrayList<String> list = createArrayList();
+        list.ensureCapacity(capacity);
+
+        return list.toString().equals("[a, null, b, c, d, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]");
+    }
+
+    public static boolean testTrimToSize() {
+        ArrayList<String> list = createArrayList();
+        list.trimToSize();
+
+        return list.toString().equals("[a, null, b, c, d]");
+    }
+
+    public static boolean testToArray() {
+        ArrayList<String> list = createArrayList();
+        String expected = "[a, null, b, c, d]";
+
+        return Objects.equals(Arrays.toString(list.toArray()), expected);
+    }
+
+    public static boolean testContainsContainingElement() {
+        ArrayList<String> list = createArrayList();
+
+        return list.contains("d");
+    }
+
+    public static boolean testContainNotContainingElement() {
+        ArrayList<String> list = createArrayList();
+
+        return !list.contains("f");
+    }
+
+    public static boolean testIndexOfContainingElement() {
+        ArrayList<String> list = createArrayList();
+
+        return list.indexOf("b") == 2;
+    }
+
+    public static boolean testIndexOfNotContainingElement() {
+        ArrayList<String> list = createArrayList();
+
+        return list.indexOf("f") == -1;
+    }
+
+    public static boolean testLastIndexOfContainingElement() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        list.add(null);
+        list.add("b");
+        list.add("d");
+        list.add("d");
+
+        return list.lastIndexOf("d") == 4;
+    }
+
+    public static boolean testLastIndexOfNotContainingElement() {
+        ArrayList<String> list = createArrayList();
+
+        return list.lastIndexOf("f") == -1;
+    }
+
+    public static boolean testRemoveObjectOnList() {
+        ArrayList<String> list = createArrayList();
+        return list.remove("b");
+    }
+
+    public static boolean testRemoveObjectOutList() {
+        ArrayList<String> list = createArrayList();
+        return !list.remove("f");
+    }
+
+    public static boolean testContainsAllContainingElements() {
+        ArrayList<String> list = createArrayList();
+
+        List<String> collection = new java.util.ArrayList<>();
+        collection.add("d");
+        collection.add("b");
+
+        return list.containsAll(collection);
+    }
+
+    public static boolean testContainsAllNotContainingElements() {
+        ArrayList<String> list = createArrayList();
+
+        List<String> collection = new java.util.ArrayList<>();
+        collection.add("d");
+        collection.add("f");
+
+        return !list.containsAll(collection);
+    }
+
+    public static boolean testAddAll() {
+        ArrayList<String> list = createArrayList();
+
+        List<String> collection = new java.util.ArrayList<>();
+        collection.add("d");
+        collection.add("f");
+
+        list.addAll(collection);
+
+        return list.toString().equals("[a, null, b, c, d, d, f, null, null, null]");
+    }
+
+    public static boolean testAddAllByIndex() {
+        ArrayList<String> list = createArrayList();
+
+        List<String> collection = new java.util.ArrayList<>();
+        collection.add("f");
+        collection.add("f");
+
+        int index = 1;
+        list.addAll(index, collection);
+
+        return list.toString().equals("[a, f, f, null, b, c, d, null, null, null]");
+    }
+
+    public static boolean testRemoveAll() {
+        ArrayList<String> list = createArrayList();
+
+        List<String> collection = new java.util.ArrayList<>();
+        collection.add("d");
+        collection.add("f");
+
+        list.removeAll(collection);
+
+        return list.toString().equals("[a, null, b, c, null, null, null, null, null, null]");
+    }
+
+    public static boolean testRetainsAllOnList() {
+        ArrayList<String> list = createArrayList();
+
+        List<String> collection = new java.util.ArrayList<>();
+        collection.add("c");
+        collection.add("d");
+
+        return list.retainAll(collection);
+    }
+
+    public static boolean testRetainsAllOutList() {
+        ArrayList<String> list = createArrayList();
+
+        List<String> collection = new java.util.ArrayList<>();
+        collection.add("f");
+        collection.add("g");
+
+        return list.retainAll(collection);
+    }
+
+    public static boolean testClear() {
+        ArrayList<String> list = createArrayList();
+        list.clear();
+
+        return list.toString().equals("[null, null, null, null, null, null, null, null, null, null]");
+    }
+
+    public static boolean testGet() {
+        ArrayList<String> list = createArrayList();
+
+        return list.get(2).equals("b");
+    }
+
+    public static boolean testSet() {
+        ArrayList<String> list = createArrayList();
+        list.set(2, "bb");
+
+        return list.toString().equals("[a, null, bb, c, d, null, null, null, null, null]");
+    }
+
+    public static void runTests() {
+        System.out.printf("%b : проверка add %n", testAdd());
+        System.out.printf("%b : Проверка size%n", testSize());
+        System.out.printf("%b : Проверка toString%n", testToString());
+        System.out.printf("%b : Проверка addByIndexFirst%n", testAddFirst());
+        System.out.printf("%b : Проверка addByIndexLast%n", testAddLast());
+        System.out.printf("%b : Проверка remove by index%n", testRemoveByIndex());
+        System.out.printf("%b : Проверка ensureCapacity%n", testEnsureCapacity());
+        System.out.printf("%b : Проверка trimToSize%n", testTrimToSize());
+        System.out.printf("%b : Проверка ToArray%n", testToArray());
+        System.out.printf("%b : Проверка contains (содержит)%n", testContainsContainingElement());
+        System.out.printf("%b : Проверка contains (не содержит)%n", testContainNotContainingElement());
+        System.out.printf("%b : Проверка indexOf (содержит)%n", testIndexOfContainingElement());
+        System.out.printf("%b : Проверка indexOf (не содержит)%n", testIndexOfNotContainingElement());
+        System.out.printf("%b : Проверка lastIndexOf (содержит)%n", testLastIndexOfContainingElement());
+        System.out.printf("%b : Проверка lastIndexOf (не содержит)%n", testLastIndexOfNotContainingElement());
+        System.out.printf("%b : Проверка remove by object%n", testRemoveObjectOnList());
+        System.out.printf("%b : Проверка remove by object%n", testRemoveObjectOutList());
+        System.out.printf("%b : Проверка containsAll (содержит)%n", testContainsAllContainingElements());
+        System.out.printf("%b : Проверка containsAll (не содержит)%n", testContainsAllNotContainingElements());
+        System.out.printf("%b : Проверка addAll%n", testAddAll());
+        System.out.printf("%b : Проверка addAll by index%n", testAddAllByIndex());
+        System.out.printf("%b : Проверка removeAll%n", testRemoveAll());
+        System.out.printf("%b : Проверка retainsAll (содержит)%n", testRetainsAllOnList());
+        System.out.printf("%b : Проверка retainsAll (не содержит)%n", testRetainsAllOutList());
+        System.out.printf("%b : проверка clear %n", testClear());
+        System.out.printf("%b : проверка get %n", testGet());
+        System.out.printf("%b : проверка set %n", testSet());
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Реализация ArrayList");
+
+        runTests();
+    }
 }
